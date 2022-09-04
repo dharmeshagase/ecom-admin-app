80a847017b6434fa4958f606f0fe8f7a34736725
Dharmesh Agase <dharmeshagase@gmail.com>
2022-09-04 18:38:33 +0530

    Solved the bug in baseUrl

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 
 // const baseUrl = location.hostname === "localhost"?"http://localhost:2000": "https://flipkart-admin-website.herokuapp.com"
-const baseUrl = "https://flipkart-backend-rest-server.herokuapp.com/";
+const baseUrl = "https://flipkart-backend-rest-server.herokuapp.com";
 export const api = `${baseUrl}/api`;
 
 export const generatePublicImageUrl = (filename)=>{
