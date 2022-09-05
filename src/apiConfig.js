e09aa8b78d8bc7f96b63fc7f69884a0355880984
Dharmesh Agase <dharmeshagase@gmail.com>
2022-09-05 19:05:13 +0530

    Added the baseUrl to the deployed heroku backend

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 
 // const baseUrl = location.hostname === "localhost"?"http://localhost:2000": "https://flipkart-admin-website.herokuapp.com"
-// const baseUrl = "https://flipkart-backend-rest-server.herokuapp.com";
-const baseUrl = "http://localhost:2000";
+const baseUrl = "https://flipkart-backend-rest-server.herokuapp.com";
+// const baseUrl = "http://localhost:2000";
 export const api = `${baseUrl}/api`;
 
 // export const generatePublicImageUrl = (filename)=>{
