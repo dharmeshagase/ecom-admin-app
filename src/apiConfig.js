58ede3c39cdebd8009db34d17865f8163196d0ad
dharmeshagase <dharmeshagase@gmail.com>
2023-02-28 17:50:05 +0530

    BaseURL change from heroku to render domain

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 
 // const baseUrl = location.hostname === "localhost"?"http://localhost:2000": "https://flipkart-admin-website.herokuapp.com"
-const baseUrl = "https://flipkart-backend-rest-server.herokuapp.com";
+const baseUrl = "https://https://ecom-backend-rest-server.onrender.com";
 // const baseUrl = "http://localhost:2000";
 export const api = `${baseUrl}/api`;
 
